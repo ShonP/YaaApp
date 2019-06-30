0090da9f30b7046567809d69b53bbb2722d60574
Shon <shonp@welldone-software.com>
2019-06-30 09:00:54 +0300

    adding images

--- src/pages/unit-gallery/unit-gallery.ts
+++ src/pages/unit-gallery/unit-gallery.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import {
   IonicPage,
   NavController,
   NavParams,
   ViewController
-} from "ionic-angular";
+} from 'ionic-angular';
 
 /**
  * Generated class for the GalleryPage page.
@@ -14,20 +14,37 @@ import {
  */
 
 @Component({
-  selector: "page-unit-gallery",
-  templateUrl: "unit-gallery.html"
+  selector: 'page-unit-gallery',
+  templateUrl: 'unit-gallery.html'
 })
 export class UnitGalleryPage implements OnInit {
   images = [];
-  name: string = "";
+  name: string = '';
   constructor(
     private viewCtrl: ViewController,
     public navCtrl: NavController,
     public navParams: NavParams
   ) {}
   ngOnInit() {
-    this.images = ["1.jpg","2.jpg","3.jpg","yaa1.jpg", "yaa2.jpg","yaa3.jpg"];
-    this.name = "גלריה";
+    this.images = [
+      '1.jpg',
+      '2.jpg',
+      '3.jpg',
+      'yaa1.jpg',
+      'yaa2.jpg',
+      'yaa3.jpg',
+      '4.png',
+      '5.png',
+      '6.jpg',
+      '7.jpg',
+      '8.jpg',
+      '9.jpg',
+      '10.png',
+      '11.jpg',
+      '12.png',
+      '13.jpg'
+    ];
+    this.name = 'גלריה';
   }
   dismiss() {
     this.navCtrl.pop();
